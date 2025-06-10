74ff5bdd2b5439faa5698b79cd1492da57948e75
yamil <carlosyamil81@gmail.com>
2025-06-10 13:49:37 -0300

    Primer commit - Base funcional con configuración Spring Boot

---
+++ src/main/java/com/avanzapp/avanzapp/AvanzappApplication.java
@@ -0,0 +1,13 @@
+package com.avanzapp.avanzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanzappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanzappApplication.class, args);
+		System.out.print("Hola, comenzambfgvvos este nuevo emprendimiento  yo");
+	}
+}
---
+++ src/test/java/com/avanzapp/avanzapp/AvanzappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanzapp.avanzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanzappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
