f44bc42f006574a4b86d6e1b0f693e2ce9d70bed
Rodrigo Ponce <rodrigoponce@pm.me>
2025-11-15 12:45:13 -0300

    Se soluciona el error por modo lazy para los detalles y se actualiza el postman para test

--- src/main/java/com/avanzapp/avanzapp/repository/DNITVentasCabeceraRepository.java
+++ src/main/java/com/avanzapp/avanzapp/repository/DNITVentasCabeceraRepository.java
@@ -1,6 +1,7 @@
 package com.avanzapp.avanzapp.repository;
 
 import com.avanzapp.avanzapp.model.DNITVentasCabecera;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,10 +9,13 @@ import java.util.Optional;
 
 public interface DNITVentasCabeceraRepository extends JpaRepository<DNITVentasCabecera, Long> {
 
+    @EntityGraph(attributePaths = "detalles")
     List<DNITVentasCabecera> findByUsuarioId(Long usuarioId);
 
+    @EntityGraph(attributePaths = "detalles")
     List<DNITVentasCabecera> findByUsuarioIdAndPeriodoEmision(Long usuarioId, String periodoEmision);
 
+    @EntityGraph(attributePaths = "detalles")
     List<DNITVentasCabecera> findByUsuarioIdAndPeriodoMesAndPeriodoAnio(Long usuarioId, Integer periodoMes, Integer periodoAnio);
 
     Optional<DNITVentasCabecera> findFirstByUsuarioIdAndPeriodoMesAndPeriodoAnioAndNumeroComprobante(
